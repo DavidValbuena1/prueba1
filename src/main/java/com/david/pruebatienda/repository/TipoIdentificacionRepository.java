57f2ae4ac401cc137ce1e33ad6966785db79d31f
DavidValbuena1 <104107409+DavidValbuena1@users.noreply.github.com>
2022-08-23 21:05:24 -0500

    Add files via upload

--- src/main/java/com/david/pruebatienda/repository/TipoIdentificacionRepository.java
+++ src/main/java/com/david/pruebatienda/repository/TipoIdentificacionRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.repository.Repository;
 
 import com.david.pruebatienda.model.TipoIdentificacion;
 
-@org.springframework.stereotype.Repository
 public interface TipoIdentificacionRepository extends Repository<TipoIdentificacion, Integer> {
 	public List<TipoIdentificacion> findAll();
 	public TipoIdentificacion findById(int id);
--- src/main/java/com/david/pruebatienda/serviceimp/TipoIdentificacionImp.java
+++ src/main/java/com/david/pruebatienda/serviceimp/TipoIdentificacionImp.java
@@ -6,22 +6,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.david.pruebatienda.model.TipoIdentificacion;
+import com.david.pruebatienda.repository.TipoIdentificacionRepository;
 import com.david.pruebatienda.service.STipoIdentificacion;
 
 @Component
 public class TipoIdentificacionImp implements STipoIdentificacion {
 
 	@Autowired(required=true)
-	private STipoIdentificacion service;
+	private TipoIdentificacionRepository service;
 	
 	@Override
 	public List<TipoIdentificacion> encontrarTodos() {
-		return service.encontrarTodos();
+		return service.findAll();
 	}
 
 	@Override
 	public TipoIdentificacion encontrarPorId(int id) {
-		return service.encontrarPorId(id);
+		return service.findById(id);
 	}
 
 }
