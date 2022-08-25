2c3a7bcc6b4c2b5ca22d3fdeca009610238e5efa
DavidValbuena1 <104107409+DavidValbuena1@users.noreply.github.com>
2022-08-25 14:47:49 -0500

    Add files via upload

--- src/main/java/com/david/pruebatienda/model/Bodega.java
+++ src/main/java/com/david/pruebatienda/model/Bodega.java
@@ -4,8 +4,6 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -20,7 +18,6 @@ public class Bodega implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
 	@Column
--- src/main/java/com/david/pruebatienda/model/BodegaProducto.java
+++ src/main/java/com/david/pruebatienda/model/BodegaProducto.java
@@ -1,8 +1,11 @@
 package com.david.pruebatienda.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.IdClass;
 import javax.persistence.JoinColumn;
@@ -11,7 +14,6 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name="bodegas_has_productos")
-@IdClass(BodegaProductoId.class)
 public class BodegaProducto implements Serializable{
 	/**
 	 * 
@@ -19,14 +21,17 @@ public class BodegaProducto implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@ManyToOne
+	@JoinColumn(name="productos_id")
+	private Producto idProducto;
+	
 	@ManyToOne
 	@JoinColumn(name="bodegas_id")
 	private Bodega idBodega;
 	
-	@Id
-	@ManyToOne
-	@JoinColumn(name="productos_id")
-	private Producto idProducto;
 
 	public Bodega getIdBodega() {
 		return idBodega;
@@ -47,6 +52,35 @@ public class BodegaProducto implements Serializable{
 	@Override
 	public String toString() {
 		return "BodegaProducto [idBodega=" + idBodega + ", idProducto=" + idProducto + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(idBodega, idProducto);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BodegaProducto other = (BodegaProducto) obj;
+		return Objects.equals(idBodega, other.idBodega) && Objects.equals(idProducto, other.idProducto);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
 	}
 	
 	
